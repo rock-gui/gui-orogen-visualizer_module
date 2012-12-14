ca6a5bf855e346f426d50c6f9d993463c43eb1cf
Stefan Haase <stefan.haase@dfki.de>
2012-12-14 12:27:05 +0100

    Adds start scripts to connect to (and visualize) CREX in simulation and the real Sherpa.

---
+++ scripts/run_sherpa_visualization.rb
@@ -0,0 +1,38 @@
+require 'orocos'
+require 'readline'
+
+include Orocos
+
+Nameservice::enable(:CORBA, :host => "10.250.8.7") # Sherpa PC
+
+Orocos.initialize
+
+Orocos.run 'visualizer_module_deployment',
+	    "valgrind" => false, "wait" => 2000, "output" => nil do |*_|
+
+    visualizer = TaskContext.get('visualizer_module')
+    visualizer.configure
+    visualizer.start
+
+    pointcloud = TaskContext.get('pointcloud_creator')
+    planner = TaskContext.get('simple_path_planner')
+    follower = TaskContext.get 'trajectory'
+
+    pointcloud.envire_environment_out.connect_to(visualizer.envire_environment_in)
+    planner.trajectory_out.connect_to(visualizer.trajectory_in)
+    planner.trajectory_spline_out.connect_to(visualizer.trajectory_spline_in)
+    follower.motion_command.connect_to(visualizer.motion_command_in)
+
+    # Send pose to modules (will be replaced by Vicon)
+    visualizer_pose_writer = visualizer.robot_pose_in.writer
+    
+    rbs = visualizer_pose_writer.new_sample
+    rbs.sourceFrame = "robot"
+    rbs.targetFrame = "world"
+    rbs.position = Eigen::Vector3.new(0.0, 0.0, 0.0)
+    rbs.orientation = Eigen::Quaternion.Identity
+    while true
+        visualizer_pose_writer.write(rbs)
+        sleep 1
+    end
+end
---
+++ scripts/run_simu_visualization.rb
@@ -0,0 +1,29 @@
+require 'orocos'
+require 'readline'
+
+include Orocos
+
+Nameservice::enable(:CORBA, :host => "127.0.0.1")
+
+Orocos.initialize
+
+Orocos.run 'visualizer_module_deployment',
+	    "valgrind" => false, "wait" => 2000, "output" => nil do |*_|
+
+    visualizer = TaskContext.get('visualizer_module')
+    visualizer.configure
+    visualizer.start
+
+    crex_simu = TaskContext.get 'test_crex_simulation'
+    pointcloud = TaskContext.get 'pointcloud_creator'
+    planner = TaskContext.get 'simple_path_planner'
+    follower = TaskContext.get 'trajectory'
+
+    crex_simu.pose_out.connect_to(visualizer.robot_pose_in)
+    pointcloud.envire_environment_out.connect_to(visualizer.envire_environment_in)
+    planner.trajectory_out.connect_to(visualizer.trajectory_in)
+    planner.trajectory_spline_out.connect_to(visualizer.trajectory_spline_in)
+    follower.motion_command.connect_to(visualizer.motion_command_in)
+
+    Readline::readline("Press ENTER to exit ...")
+end
